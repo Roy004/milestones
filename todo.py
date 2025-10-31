166fc664dbd1a5ad49a0e60284300378ce6163cf
Roy004 <roinelperez5@gmail.com>
2025-10-30 20:53:20 -0400

    completado to_dict

--- todo.py
+++ todo.py
@@ -1,18 +1,34 @@
 from datetime import datetime
+import json
 
 
 class Tarea:
-    def __init__(self, texto:str, completada:bool, prioridad:int, fecha_creada:datetime) -> None:
+    def __init__(self, texto:str, completada:bool, prioridad:str) -> None:
         self.texto=texto
         self.completada=completada
         self.prioridad=prioridad
-        self.fecha_creada=fecha_creada
+        self.fecha_creada=datetime.now()
+
+    def to_dict(self):
+        return {
+            'texto':self.texto,
+            'completada':self.completada,
+            'prioridad':self.prioridad,
+            'fecha_creacion':self.fecha_creada.isoformat()
+        }
 
 class Lista:
 
-    def __init__(self) -> None:
-        self.tareas=[]
+    def agregar_tarea(self, tarea:Tarea):
+        with open('tareas.json','r+',encoding='utf-8') as tareas:
+            for t in tareas:
+                if t==tarea:
+                    print('ya existe esta tarea')
+                    return
+            json.dump(tarea.to_dict(), tareas)
+        return True
 
-    def agregar_tarea(self, ):
-        pass
-    
+
+lista=Lista()
+
+lista.agregar_tarea(Tarea('Ir a buscar el pollo',False,'Alta'))
