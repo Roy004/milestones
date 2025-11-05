562be0ecdaa3ee5c21f55e2535147c9fc8ed158b
Roy004 <roinelperez5@gmail.com>
2025-11-05 09:23:20 -0500

    eliminado el proyecto django

--- ecommerce_rest/products_api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Color, Producto
-
-admin.site.register(Producto)
-admin.site.register(Color)
-
-# Register your models here.
--- ecommerce_rest/products_api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-url_patterns = [
-    path('', views.bienvenida, name='bienvenida'),
-    path('listar_productos/', views.listar_productos, name='listar_productos'),
-]
